0b89ae5e79fca1d6647aa2f7db82450e3da57d21
Tomas McGuinness <tomas@tomasmcguinness.com>
2013-04-28 20:49:04 +0100

    Got simple retrieval of search and reviews working.

---
+++ AnswerAReview.Web/Controllers/AppController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace AnswerAReview.Web.Controllers
+{
+    public class AppController : Controller
+    {
+        public ActionResult Index(long appId)
+        {
+            return View(appId);
+        }
+    }
+}
