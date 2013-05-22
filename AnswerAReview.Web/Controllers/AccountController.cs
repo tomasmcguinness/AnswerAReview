eba8db1e60e456d8bbd5522040050d8576090158
Tomas McGuinness <tomas@chewedpencil.net>
2013-05-22 15:31:59 +0100

    Updated and added an Account page.

---
+++ AnswerAReview.Web/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace AnswerAReview.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index(long appId)
+        {
+            return View();
+        }
+    }
+}
